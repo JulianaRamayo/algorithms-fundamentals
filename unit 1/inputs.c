84075b57a714b87e6453a6f4e23f6e1d53c3846f
Juliana Ramayo <71157416+JulianaRamayo@users.noreply.github.com>
2022-01-27 23:03:54 -0600

    Add files via upload

---
+++ unit 1/inputs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int num1; //Declarando mi variable
+int num2;
+
+int main(){
+
+    //Imprimir un mensaje//
+    printf("Enter number1: \n\tnum1: ");
+    scanf("%d", &num1);
+    printf("Enter number2: \n\tnum2: ");
+    scanf("%d", &num2);
+    return 0;
+}
