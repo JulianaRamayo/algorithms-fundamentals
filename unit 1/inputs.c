a71df374d801fdea8187ded7210ae801432c97b6
Juliana Ramayo <71157416+JulianaRamayo@users.noreply.github.com>
2022-01-31 16:38:17 -0600

    Add files via upload

--- unit 1/inputs.c
+++ unit 1/inputs.c
@@ -4,11 +4,15 @@ int num1; //Declarando mi variable
 int num2;
 
 int main(){
-
+    
     //Imprimir un mensaje//
     printf("Enter number1: \n\tnum1: ");
-    scanf("%d", &num1);
-    printf("Enter number2: \n\tnum2: ");
+    //* Inputs *//
+    scanf("%d", &num1); //Inputs Keyboard
+    printf("Enter number2: \n\tnum2: "); //Outputs Screen
     scanf("%d", &num2);
+    //*Procesos*//
+    int sum = num1 + num2;
+    printf("suma = %d\n", sum);
     return 0;
 }
