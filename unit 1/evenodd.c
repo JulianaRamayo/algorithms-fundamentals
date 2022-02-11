345123f888faf92e2e89f77c3300b1f054207dcb
JulianaRamayo <JulianaRamayo>
2022-02-11 11:27:29 -0600

    added task4

---
+++ unit 1/evenodd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    int numberToTest = atoi(argv[1]);
+    bool isDividedByTwo = (numberToTest % 2)
+
+    
+    
+    return 0;
+}
