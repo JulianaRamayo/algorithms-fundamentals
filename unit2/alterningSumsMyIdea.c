fd28636611269896a3c3d3857ae5251dd3af55c4
JulianaRamayo <JulianaRamayo>
2022-03-09 10:39:58 -0600

    alterningSumsMaestro added

---
+++ unit2/alterningSumsMyIdea.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int i = 0;
+
+int main(){
+    int weight[5];
+    printf("Enter people's weight");
+    for (int i = 0; i < 5; i + 1){
+        scanf("%d\n", &weight[i]);
+    }
+    
+    printf("Alterning sums");
+    printf("Team 1");
+    for (i = 0; i + 2){
+        int sum1 = i
+    }
+}
